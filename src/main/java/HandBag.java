e23476c3498284fd13e19efbd5379b0f2f958781
Lisa Nas-Yu <lisa.nasuyu@gmail.com>
2022-09-21 22:53:23 -0400

    test push

--- src/main/java/HandBag.java
+++ src/main/java/HandBag.java
@@ -24,7 +24,7 @@ public class HandBag extends Bag{
     }
 
     /**
-     * Increase the capacity of this bag by 1.
+     * Increase the capacity of this bag by 1
      */
     @Override
     public void enhance() {
